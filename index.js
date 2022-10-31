23095efe2934af1fc043b85c66221b5f26b646e2
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2022-10-31 10:55:29 +0100

    changed username

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   const obj = {
-    "slackUsername": "Kehinde Jejelaye",
+    "slackUsername": "kJejelaye",
     "backend": true,
     "age": 26,
     "bio": "I am a backend developer currently living in Lagos, Nigeria. I am interested in Web development, mobile development, data structures and algorithms. I strongly desire to be challenged and to test my limits - and that is why I'm here."
